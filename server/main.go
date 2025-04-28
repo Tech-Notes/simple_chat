be6e95998709a3378e27c1825962046aed963284
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-04-28 18:59:48 +0700

    feat: handle multi client concurrently

--- server/main.go
+++ server/main.go
@@ -1,48 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "github.com/gorilla/websocket"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 // Upgrader is used to upgrade HTTP connections to WebSocket connections.
 var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool {
-       return true
-    },
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-    // Upgrade the HTTP connection to a WebSocket connection
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-       fmt.Println("Error upgrading:", err)
-       return
-    }
-    defer conn.Close()
-    // Listen for incoming messages
-    for {
-       // Read message from the client
-       _, message, err := conn.ReadMessage()
-       if err != nil {
-          fmt.Println("Error reading message:", err)
-          break
-       }
-       fmt.Printf("Received: %s\\n", message)
-       // Echo the message back to the client
-       if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
-          fmt.Println("Error writing message:", err)
-          break
-       }
-    }
+	// Upgrade the HTTP connection to a WebSocket connection
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading:", err)
+		return
+	}
+
+	// Listen for incoming messages concurrently
+   go handleConnection(conn)
+
+}
+
+func handleConnection(conn *websocket.Conn) {
+   defer conn.Close()
+
+	for {
+		// Read message from the client
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			break
+		}
+		fmt.Printf("Received: %s\\n", message)
+		// Echo the message back to the client
+		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			fmt.Println("Error writing message:", err)
+			break
+		}
+	}
 }
 
 func main() {
-    http.HandleFunc("/ws", wsHandler)
-    fmt.Println("WebSocket server started on :8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-       fmt.Println("Error starting server:", err)
-    }
-}	
+	http.HandleFunc("/ws", wsHandler)
+	fmt.Println("WebSocket server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
